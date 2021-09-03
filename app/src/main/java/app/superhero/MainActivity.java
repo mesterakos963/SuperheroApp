c7bd28c3a18613d60ad7c81eb87718a4311c1e5c
mesterakos963 <akos.mester9@gmail.com>
2021-09-03 11:20:34 +0200

    Hide navigation bar while typing

--- app/src/main/java/app/superhero/MainActivity.java
+++ app/src/main/java/app/superhero/MainActivity.java
@@ -1,8 +1,9 @@
 package app.superhero;
 
-import android.os.Bundle;
+import android.graphics.Rect;
+import android.view.View;
 
-import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.FragmentActivity;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
@@ -16,12 +17,49 @@ import org.androidannotations.annotations.ViewById;
 @EActivity(R.layout.activity_main)
 public class MainActivity extends FragmentActivity {
 
+    boolean isKeyboardShowing = false;
+
+    @ViewById
+    ConstraintLayout mainActivityRoot;
+
+
     @ViewById
     BottomNavigationView bottomNavigationView;
+
+    void onKeyboardVisibilityChanged(boolean opened) {
+        if(opened) {
+            bottomNavigationView.setVisibility(View.GONE);
+        } else {
+            bottomNavigationView.setVisibility(View.VISIBLE);
+        }
+    }
 
     @AfterViews
     public void init() {
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.navHostFragment);
         NavigationUI.setupWithNavController(bottomNavigationView, navHostFragment.getNavController());
+        mainActivityRoot.getViewTreeObserver().addOnGlobalLayoutListener(
+                () -> {
+                    Rect r = new Rect();
+                    mainActivityRoot.getWindowVisibleDisplayFrame(r);
+                    int screenHeight = mainActivityRoot.getRootView().getHeight();
+
+                    int keypadHeight = screenHeight - r.bottom;
+
+                    if (keypadHeight > screenHeight * 0.15) { // 0.15 ratio is perhaps enough to determine keypad height.
+                        // keyboard is opened
+                        if (!isKeyboardShowing) {
+                            isKeyboardShowing = true;
+                            onKeyboardVisibilityChanged(true);
+                        }
+                    }
+                    else {
+                        // keyboard is closed
+                        if (isKeyboardShowing) {
+                            isKeyboardShowing = false;
+                            onKeyboardVisibilityChanged(false);
+                        }
+                    }
+                });
     }
 }
