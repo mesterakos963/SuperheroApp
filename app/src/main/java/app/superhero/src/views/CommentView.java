16dc4101363b4bc036dd6392de5e3d8ce3ce3616
mesterakos963 <akos.mester9@gmail.com>
2021-09-29 12:01:33 +0200

    Added counter to CommentView

--- app/src/main/java/app/superhero/src/views/CommentView.java
+++ app/src/main/java/app/superhero/src/views/CommentView.java
@@ -1,17 +1,32 @@
 package app.superhero.src.views;
 
 import android.content.Context;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EViewGroup;
+import org.androidannotations.annotations.ViewById;
 
 import app.superhero.R;
 
 @EViewGroup(R.layout.comment_view)
 public class CommentView extends LinearLayout {
+
+    @ViewById
+    TextView characterCounter;
+
+    @ViewById
+    EditText commentText;
+
+    private final int CHARACTER_LIMIT = 500;
+
     public CommentView(Context context) {
         super(context);
     }
@@ -26,5 +41,34 @@ public class CommentView extends LinearLayout {
 
     public CommentView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @AfterViews
+    void init() {
+        countCharacters();
+    }
+
+    private void countCharacters() {
+        characterCounter.setText(String.valueOf(CHARACTER_LIMIT- commentText.length()));
+        commentText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                characterCounter
+                        .setText(getContext().getResources()
+                                .getQuantityString(R.plurals.character_limit_label,
+                                        CHARACTER_LIMIT - commentText.length(),
+                                        CHARACTER_LIMIT - commentText.length()));
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
     }
 }
