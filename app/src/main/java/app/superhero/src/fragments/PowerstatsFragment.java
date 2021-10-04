23a33b77c847dc5657a11a5bb70da42c750c337d
mesterakos963 <akos.mester9@gmail.com>
2021-10-04 16:58:59 +0200

    Chart design in progress

--- app/src/main/java/app/superhero/src/fragments/PowerstatsFragment.java
+++ app/src/main/java/app/superhero/src/fragments/PowerstatsFragment.java
@@ -3,10 +3,18 @@ package app.superhero.src.fragments;
 import android.graphics.Color;
 import android.graphics.Typeface;
 
+import com.github.mikephil.charting.charts.HorizontalBarChart;
 import com.github.mikephil.charting.charts.RadarChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.RadarData;
 import com.github.mikephil.charting.data.RadarDataSet;
 import com.github.mikephil.charting.data.RadarEntry;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
 import com.github.mikephil.charting.interfaces.datasets.IRadarDataSet;
 
 import org.androidannotations.annotations.AfterViews;
@@ -35,7 +43,9 @@ public class PowerstatsFragment extends BaseFragment {
     @ViewById
     RadarChart radarChart;
 
-    List<RadarEntry> entries = new ArrayList<>();
+    @ViewById
+    HorizontalBarChart barChart;
+    List<String> xAxisLabels;
 
     @AfterViews
     public void init() {
@@ -45,46 +55,107 @@ public class PowerstatsFragment extends BaseFragment {
 
     private void observePowerstats() {
         viewModel.powerstats.observe(this, powerstats -> {
-            /*intelligence.setText(Integer.toString(powerstats.getIntelligence()));
-            strength.setText(Integer.toString(powerstats.getStrength()));
-            speed.setText(Integer.toString(powerstats.getSpeed()));
-            durability.setText(Integer.toString(powerstats.getDurability()));
-            power.setText(Integer.toString(powerstats.getPower()));
-            combat.setText(Integer.toString(powerstats.getCombat()));*/
-            setData(powerstats);
+            setRadarChart(powerstats);
+            setHorizontalBarChart(powerstats);
         });
     }
 
-    private void setData(Powerstats powerstats) {
-        ArrayList<RadarEntry> entries1 = new ArrayList<>();
+    private void setHorizontalBarChart(Powerstats powerstats) {
+        Description description = barChart.getDescription();
+        List<BarEntry> values = new ArrayList<>();
 
-        entries1.add(new RadarEntry(powerstats.getIntelligence()));
-        entries1.add(new RadarEntry(powerstats.getStrength()));
-        entries1.add(new RadarEntry(powerstats.getSpeed()));
-        entries1.add(new RadarEntry(powerstats.getDurability()));
-        entries1.add(new RadarEntry(powerstats.getPower()));
-        entries1.add(new RadarEntry(powerstats.getCombat()));
+        values.add(new BarEntry(0, powerstats.getIntelligence()));
+        values.add(new BarEntry(1, powerstats.getStrength()));
+        values.add(new BarEntry(2, powerstats.getSpeed()));
+        values.add(new BarEntry(3, powerstats.getDurability()));
+        values.add(new BarEntry(4, powerstats.getPower()));
+        values.add(new BarEntry(5, powerstats.getCombat()));
 
-        RadarDataSet set1 = new RadarDataSet(entries1, "Powerstats");
-        set1.setColor(Color.rgb(255, 187, 59));
-        set1.setFillColor(Color.rgb(255, 187, 59));
-        set1.setDrawFilled(true);
-        set1.setFillAlpha(180);
-        set1.setLineWidth(2f);
-        set1.setDrawHighlightCircleEnabled(true);
-        set1.setDrawHighlightIndicators(false);
+        BarDataSet set = new BarDataSet(values, "Powerstats");
+
+        List<IBarDataSet> sets = new ArrayList<>();
+        sets.add(set);
+
+        BarData data = new BarData(sets);
+        data.setValueTypeface(Typeface.defaultFromStyle(Typeface.BOLD_ITALIC));
+        data.setValueTextSize(8f);
+        data.setDrawValues(false);
+        data.setValueTextColor(R.color.chart_text_color);
+        description.setEnabled(false);
+
+        getXAxisValues();
+        barChart.setDrawGridBackground(false);
+        barChart.setDragEnabled(false);
+        barChart.setPinchZoom(false);
+        barChart.setScaleEnabled(false);
+        barChart.isDoubleTapToZoomEnabled();
+        barChart.getAxisRight().setDrawGridLines(false);
+        barChart.getAxisLeft().setDrawGridLines(false);
+        barChart.getXAxis().setDrawGridLines(false);
+        barChart.setDrawBarShadow(false);
+        barChart.setDrawValueAboveBar(false);
+        barChart.getDescription().setEnabled(false);
+        barChart.setDrawGridBackground(false);
+        set.setColor(getContext().getResources().getColor(R.color.bar_chart_color));
+
+        barChart.getXAxis().setValueFormatter(new ValueFormatter() {
+            @Override
+            public String getAxisLabel(float value, AxisBase axis) {
+                return xAxisLabels.get((int)value);
+            }
+
+            @Override
+            public String getBarLabel(BarEntry barEntry) {
+                return super.getBarLabel(barEntry);
+            }
+        });
+
+
+        barChart.setData(data);
+        barChart.invalidate();
+    }
+
+    private List<String> getXAxisValues() {
+        xAxisLabels = new ArrayList<>();
+        xAxisLabels.add(getContext().getString(R.string.Intelligence));
+        xAxisLabels.add(getContext().getString(R.string.Strength));
+        xAxisLabels.add(getContext().getString(R.string.Speed));
+        xAxisLabels.add(getContext().getString(R.string.Durability));
+        xAxisLabels.add(getContext().getString(R.string.Power));
+        xAxisLabels.add(getContext().getString(R.string.Combat));
+        return xAxisLabels;
+    }
+
+    private void setRadarChart(Powerstats powerstats) {
+
+        ArrayList<RadarEntry> entries = new ArrayList<>();
+
+        entries.add(new RadarEntry(powerstats.getIntelligence()));
+        entries.add(new RadarEntry(powerstats.getStrength()));
+        entries.add(new RadarEntry(powerstats.getSpeed()));
+        entries.add(new RadarEntry(powerstats.getDurability()));
+        entries.add(new RadarEntry(powerstats.getPower()));
+        entries.add(new RadarEntry(powerstats.getCombat()));
+
+        RadarDataSet set = new RadarDataSet(entries, "Powerstats");
+        set.setColor(Color.rgb(255, 187, 59));
+        set.setFillColor(Color.rgb(255, 187, 59));
+        set.setDrawFilled(true);
+        set.setFillAlpha(180);
+        set.setLineWidth(2f);
+        set.setDrawHighlightCircleEnabled(true);
+        set.setDrawHighlightIndicators(false);
 
         ArrayList<IRadarDataSet> sets = new ArrayList<>();
-        sets.add(set1);
+        sets.add(set);
 
         RadarData data = new RadarData(sets);
         data.setValueTypeface(Typeface.SANS_SERIF);
         data.setValueTextSize(8f);
         data.setDrawValues(false);
-        data.setValueTextColor(Color.WHITE);
+        data.setValueTextColor(R.color.chart_text_color);
 
         radarChart.setData(data);
         radarChart.invalidate();
     }
-
 }
